165d3e2ea7be066cb31e4a58f81de005387b73df
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-11 14:44:57 -0500

    added weather object

--- js/request.js
+++ js/request.js
@@ -1,10 +1,17 @@
 
 
+// to get weather off of wheere we start, just calling request weather on San Antonio's coords.
+requestWeather({
+    lng: -98.493629,
+    lat: 29.424122
+});
+//function for getting and returning our data;
 function requestWeather(lngLat){
     let lat = lngLat.lat;
-    let lon = lngLat.lon;
+    let lon = lngLat.lng;
+
     $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/weather",
+        url: "https://api.openweathermap.org/data/2.5/forecast",
         type: "GET",
         data: {
             APPID: OPEN_WEATHERMAP_TOKEN,
@@ -14,9 +21,28 @@ function requestWeather(lngLat){
         },
         success: function(data){
             console.log(data)
+
         },
         error: function(request, textStatus, errorThrown){
             alert("Status: " + textStatus); alert("Error: " + errorThrown);
         }
     })
+}
+
+// function to be called in the success of our request;
+function sortData(data){
+
+    let weatherObject = {
+        name: data.city.name,
+        country: data.city.country,
+        forecast: data.list,
+        today: this.forecast[0],
+        day2: this.forecast[8],
+        day3: this.forecast[16],
+        day4: this.forecast[24],
+        day5: this.forecast[32],
+
+    }
+
+
 }
