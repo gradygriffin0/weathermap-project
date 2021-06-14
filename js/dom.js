ab79bbbdddf58caf4eadab4cf1dbaa94370ddf08
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-14 08:42:30 -0500

    desktop fleshed out

--- js/dom.js
+++ js/dom.js
@@ -14,14 +14,14 @@ function appendWeather(obj) {
         inner.append(`<div class="col" id="day-${index}">
                         <div class="card rounded"> 
                             <h5 class="card-header bg-primary text-white py-2">${daySort(day.dt_txt)}</h5>
-                            <img style="width:100px; height: 100px; margin: auto;" src="http://openweathermap.org/img/w/${day.weather[0].icon}.png">
+                            <img style="width:50px; height: 50px; margin: 10px auto;" src="assets/weather/${day.weather[0].icon}.svg">
                             <ul class="card-body list-unstyled">
                             <li class="currentTemp">${(day.main.temp).toFixed(0)}&deg;</li>
                                 <li>High: ${parseFloat(day.main.temp_max).toFixed(0)}&deg; </li>
                                 <li>Low: ${parseFloat(day.main.temp_min).toFixed(0)}&deg; </li>
                                 <li>Humidity: ${parseFloat(day.main.humidity).toFixed(0)}&deg; </li>
                             </ul>
-                            <div class="card-footer">${day.weather[0].description}</div>
+                            <div class="card-footer bg-primary text-white">${day.weather[0].description}</div>
                         </div>
                         </div>`)
     })
