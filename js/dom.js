bae03d783f8f31bbe3c9d8bb549b1de4f8ef6f23
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-13 21:07:08 -0500

    good styling progress

--- js/dom.js
+++ js/dom.js
@@ -3,21 +3,27 @@ function appendWeather(obj) {
     house.html("");
     if ((obj.name === "") || (obj.country === "")) {
         alert("Oops, Couldn't find that area's name");
-        house.append(`<h1>Weather for that area:</h1> <div class="row row-cols-5" id="forecastInner">
+        house.append(`<h1 class="display-4">Weather for that area:</h1> <div class="row row-cols-5" id="forecastInner">
             </div>`);
     } else {
-        house.append(`<h1>Weather in: ${obj.name}, ${obj.country}</h1><div class="row row-cols-5" id="forecastInner">
+        house.append(`<h1 class="display-4 m-4 " >${obj.name}, ${obj.country}</h1><div class="row row-cols-5" id="forecastInner">
             </div>`);
     }
     let inner = $('#forecastInner');
     obj.forecast.forEach(function (day, index) {
         inner.append(`<div class="col" id="day-${index}">
-                        <div class="card"> 
+                        <div class="card rounded"> 
                             <h5 class="card-header bg-primary text-white py-2">${daySort(day.dt_txt)}</h5>
                             <img style="width:100px; height: 100px; margin: auto;" src="http://openweathermap.org/img/w/${day.weather[0].icon}.png">
-                            <p class="card-body">temp: ${day.main.temp}, high: ${day.main.temp_max}, low: ${day.main.temp_min}, humidity: ${day.main.humidity}</p>
+                            <ul class="card-body list-unstyled">
+                            <li class="currentTemp">${day.main.temp}&deg;</li>
+                                <li>High: ${day.main.temp_max}&deg; </li>
+                                <li>Low: ${day.main.temp_min}&deg; </li>
+                                <li>Humidity: ${day.main.humidity}&deg; </li>
+                            </ul>
                         </div>
                         </div>`)
     })
+
 
 }
